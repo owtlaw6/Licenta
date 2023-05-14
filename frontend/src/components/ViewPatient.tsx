527d8602bc099b9bb870a494f32e259e0a8c6a37
owtlaw6 <44997179+owtlaw6@users.noreply.github.com>
2023-05-14 17:40:20 +0300

    edits

---
+++ frontend/src/components/ViewPatient.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ViewPatient = () => {
+  return (
+    <>
+      <iframe
+        src="http://192.168.101.18/"
+        title="Dash App"
+        width="100%"
+        height="500px"
+        style={{border: 'none'}}
+      />
+    </>
+  );
+};
+
+export default ViewPatient;
